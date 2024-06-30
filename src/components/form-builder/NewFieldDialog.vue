2d7aa831a4c5ac96e3b93a63d2a9cd707a59f85c
Ricardo Barbosa <ric.brasiliano@outlook.com>
2024-06-30 14:47:36 -0300

    Added logic to disable save button on new field dialog

--- src/components/form-builder/NewFieldDialog.vue
+++ src/components/form-builder/NewFieldDialog.vue
@@ -5,7 +5,9 @@
         <span class="headline">{{ dialogTitle }}</span>
       </v-card-title>
       <v-card-subtitle>
-        <span class="text-subtitle-1 text-justify">{{ dialogDescription }}</span>
+        <span class="text-subtitle-1 text-justify">{{
+          dialogDescription
+        }}</span>
       </v-card-subtitle>
       <v-card-text class="pb-0">
         <v-text-field
@@ -17,42 +19,44 @@
         ></v-text-field>
       </v-card-text>
       <v-card-text class="pt-0 pb-0">
-        <template v-if="fieldOptions.length">
-          <v-radio-group v-model="selectedOption">
-            <v-radio
-              v-for="option in fieldOptions"
-              :key="option"
-              :label="option"
-              :value="option"
-            ></v-radio>
-          </v-radio-group>
-        </template>
-        <template v-if="selectedFieldType === 'selection'">
-          <div>
-            <v-text-field
-              v-for="(option, index) in customOptions"
-              :key="index"
-              :label="'Opção ' + (index + 1)"
-              v-model="customOptions[index]"
-              dense
-              outlined
-            ></v-text-field>
-            <v-btn
-              text
-              color="primary"
-              @click="addOption"
-              :disabled="customOptions.length >= 4"
-            >
-              Adicionar opção
-            </v-btn>
-            <v-alert v-if="customOptions.length >= 4" type="error" dense>
-              Você só pode adicionar até 4 opções.
-            </v-alert>
-          </div>
-        </template>
+        <v-radio-group v-if="fieldOptions.length" v-model="selectedOption">
+          <v-radio
+            v-for="option in fieldOptions"
+            :key="option"
+            :label="option"
+            :value="option"
+          ></v-radio>
+        </v-radio-group>
+        <div v-if="isSelectionFieldType">
+          <v-text-field
+            v-for="(option, index) in customOptions"
+            :key="index"
+            :label="'Opção ' + (index + 1)"
+            v-model="customOptions[index]"
+            dense
+            outlined
+          ></v-text-field>
+          <v-btn
+            text
+            color="primary"
+            @click="addOption"
+            :disabled="customOptions.length >= 4"
+          >
+            Adicionar opção
+          </v-btn>
+          <v-alert v-if="customOptions.length >= 4" type="error" dense>
+            Você só pode adicionar até 4 opções.
+          </v-alert>
+        </div>
       </v-card-text>
       <v-card-actions>
-        <v-btn text color="primary" @click="saveField">Salvar</v-btn>
+        <v-btn
+          :disabled="!enableSaveButton"
+          text
+          color="primary"
+          @click="saveField"
+          >Salvar</v-btn
+        >
         <v-btn text color="red darken-1" @click="closeDialog">Cancelar</v-btn>
       </v-card-actions>
     </v-card>
@@ -91,6 +95,14 @@ export default {
         ? this.fieldTypes[this.selectedFieldType].options
         : [];
     },
+    enableSaveButton() {
+      return (
+        this.fieldLabel && (!this.fieldOptions.length || this.selectedOption)
+      );
+    },
+    isSelectionFieldType() {
+      return this.selectedFieldType === "selection";
+    },
   },
   methods: {
     open(type) {
@@ -115,7 +127,9 @@ export default {
           type: this.selectedFieldType,
           label: this.fieldLabel,
           option: this.selectedOption,
-          customOptions: this.customOptions.filter((option) => option.trim() !== "")
+          customOptions: this.customOptions.filter(
+            (option) => option.trim() !== ""
+          ),
         };
         this.$emit("field-saved", field);
         this.closeDialog();
