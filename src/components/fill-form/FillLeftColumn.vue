5e83c7f988e2ee6a95f1c65b33476b1b7e3a925c
Ricardo Barbosa <ric.brasiliano@outlook.com>
2024-06-30 18:11:19 -0300

    Created fill-form left column

---
+++ src/components/fill-form/FillLeftColumn.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <v-btn width="100%" outlined @click="$emit('new-form')" color="primary">Novo Formulário</v-btn>
+      <v-list dense>
+        <v-list-item
+          v-for="(form, index) in forms"
+          :key="index"
+          @click="selectForm(form)"
+          :class="{ 'active-form': form === selectedForm }"
+        >
+          <v-list-item-title>{{ form.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </div>
+  </template>
+  
+  <script>
+  import { useFormStore } from '@/stores/formStore';
+  
+  export default {
+    name: 'BuilderLeftColumn',
+    props: {
+      forms: Array,
+      selectedForm: Object,
+    },
+    computed: {
+      formStore() {
+        return useFormStore();
+      },
+    },
+    methods: {
+      selectForm(form) {
+        this.formStore.selectForm(form);
+        this.$emit('form-selected', form);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .active-form {
+    background-color: #f0f0f0;
+  }
+  </style>
+  
--- src/views/FillFormView.vue
+++ src/views/FillFormView.vue
@@ -1,9 +1,41 @@
 <template>
-    <h1>Form List</h1>
-  </template>
-  
-  <script>
-  export default {
-    name: "FillFormView",
-  };
-  </script>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="2">
+        <FillLeftColumn />
+      </v-col>
+
+      <v-col cols="2" class="fill-height">
+        <v-divider vertical></v-divider>
+      </v-col>
+
+      <v-col cols="6">
+        <!-- o resto aqui -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import FillLeftColumn from "@/components/fill-form/FillLeftColumn.vue";
+import { useFormStore } from "@/stores/formStore";
+
+export default {
+  name: "FillFormView",
+  components: {
+    FillLeftColumn,
+  },
+  data() {
+    return {
+      formStore: useFormStore(),
+      showNewFormComponent: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.fill-height {
+  height: 100vh;
+}
+</style>
